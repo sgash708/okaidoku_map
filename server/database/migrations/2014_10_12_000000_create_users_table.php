c44207f7777b147ba318a25528519cbf15b348d5
sgash708 <sugash7081023@gmail.com>
2021-07-30 18:47:37 +0900

    マイグレーション実行

--- server/database/migrations/2014_10_12_000000_create_users_table.php
+++ server/database/migrations/2014_10_12_000000_create_users_table.php
@@ -8,26 +8,31 @@ class CreateUsersTable extends Migration
 {
     /**
      * Run the migrations.
-     *
-     * @return void
      */
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->unsignedBigInteger('id')->comment('ユーザID');
+            $table->string('handle_name')->unique()->comment('ハンドルネーム');
+            $table->string('last_name')->comment('姓');
+            $table->string('last_name_kana')->comment('姓カナ');
+            $table->string('first_name')->comment('名');
+            $table->string('first_name_kana')->comment('名カナ');
+            $table->string('phone_number')->comment('電話番号');
+            $table->string('email')->unique()->comment('メールアドレス');
             $table->string('password');
+            $table->unsignedSmallInteger('sex')->comment('性別コード');
             $table->rememberToken();
-            $table->timestamps();
+
+            $table->dateTime('deleted_at')->softDeletes()->comment('削除日時')->nullable();
+            $table->dateTime('created_at')->comment('作成日時')->nullable();
+            $table->dateTime('updated_at')->comment('更新日時')->nullable();
+            $table->primary('id');
         });
     }
 
     /**
      * Reverse the migrations.
-     *
-     * @return void
      */
     public function down()
     {
