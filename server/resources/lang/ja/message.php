40fb4e5c94bfe9f6f355de5e5a8e6197fa6193e6
sgash708 <sugash7081023@gmail.com>
2021-08-12 10:41:16 +0900

    既存メッセージを日本語化

---
+++ server/resources/lang/ja/message.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | メッセージ出力
+    |--------------------------------------------------------------------------
+    |
+    | 登録や退会時などのバリデーションで使います。
+    | REF: https://qiita.com/mitashun/items/160784ce352cf529f853
+    |
+    */
+
+    // REF: https://github.com/terumichi1209/laravel-test/blob/develop/resources/lang/ja/message.php
+    'success_save' => '登録に成功しました。',
+    'failed_save'  => '登録に失敗しました。',
+];
