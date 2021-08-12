b10388b3804ef2bfbbe5002c15001d61958e403c
sgash708 <sugash7081023@gmail.com>
2021-08-12 13:21:26 +0900

    リポジトリクラス作成

---
+++ server/app/Repositories/UserRepository.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Entities;
+use App\Factories\UserFactory;
+
+class UserRepository
+{
+    /**
+     * インスタンス作成
+     *
+     * @param array $request_params
+     *
+     * @return Entities\User
+     */
+    public function new(array $request_params): Entities\User
+    {
+        return (new UserFactory())->make(
+            $request_params['name'],
+            $request_params['email'],
+            $request_params['password'],
+        );
+    }
+
+    /**
+     * User一覧取得
+     *
+     * @return Collection
+     */
+    public function list(): Collection
+    {
+        return User::all();
+    }
+
+    /**
+     * store関数で保存した値が正しいか
+     *
+     * @param Entities\User $user
+     * @return boolean
+     */
+    public function store(Entities\User $user): bool
+    {
+        return (new User([
+            'name' => $user->getName(),
+            'email' => $user->getEmail(),
+            'password' => $user->getPassword()
+        ]))->save();
+    }
+}
