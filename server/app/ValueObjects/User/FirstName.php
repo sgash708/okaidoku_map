7d0465a4f47ff14314078f512fd6cb15d6ba00c0
sgash708 <sugash7081023@gmail.com>
2021-08-13 13:51:04 +0900

    VO追加

---
+++ server/app/ValueObjects/User/FirstName.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\ValueObjects\User;
+
+use App\ValueObjects\BaseValueObject;
+
+class FirstName implements BaseValueObject
+{
+    /** @var string */
+    private $first_name;
+
+    /**
+     * Name constructor
+     *
+     * @param string $first_name
+     *
+     * @throws \Exception
+     */
+    public function __construct(string $first_name)
+    {
+        $this->first_name = $first_name;
+    }
+
+    /**
+     * name取得
+     *
+     * @return string
+     */
+    public function get(): string
+    {
+        return $this->first_name;
+    }
+}
