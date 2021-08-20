c3d41f3e9740a02bff05aa02bf08bf27ba5e180f
sgash708 <sugash7081023@gmail.com>
2021-08-20 15:55:01 +0900

    shopモデルのテストHasManyチェック追記

--- server/tests/Unit/Models/ShopTest.php
+++ server/tests/Unit/Models/ShopTest.php
@@ -5,13 +5,28 @@ namespace Tests\Unit\Models;
 use Tests\TestCase;
 use App\Models\Shop;
 use App\Models\ShopBusinessInfo;
-use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
-use Illuminate\Support\Facades\Schema;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
 
 class ShopTest extends TestCase
 {
-    use RefreshDatabase;
+    // データを保存させない
+    use DatabaseTransactions;
+
+    /** @var Shop */
+    private Shop $shop;
+    /** @var ShopBusinessInfo */
+    private ShopBusinessInfo $shop_business_info;
+
+    /**
+     * setUp
+     */
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->shop               = Shop::factory()->create();
+        $this->shop_business_info = ShopBusinessInfo::factory()->create(['shop_id' => $this->shop->id]);
+    }
+    
 
     /**
      * @test
@@ -19,14 +34,11 @@ class ShopTest extends TestCase
      */
     public function shopBusinessInfos()
     {
-        $shop               = Shop::factory()->create();
-        $shop_business_info = ShopBusinessInfo::factory()->create(['shop_id' => $shop->id]);
-
         // shop_bussiness_infosは、shopsのshop_business_infoのコレクションとして存在しているか
-        $this->assertTrue($shop->shopBusinessInfos->contains($shop_business_info));
+        $this->assertTrue($this->shop->shopBusinessInfos->contains($this->shop_business_info));
         // 存在するshop_business_infoデータは存在していて、数があっているか
-        $this->assertEquals(1, $shop->shopBusinessInfos->count());
+        $this->assertEquals(1, $this->shop->shopBusinessInfos->count());
         // shop_business_infosは、shopsに紐づいていてコレクションインスタンスか
-        $this->assertInstanceOf('Illuminate\Database\Eloquent\Collection', $shop->shopBusinessInfos);
+        $this->assertInstanceOf('Illuminate\Database\Eloquent\Collection', $this->shop->shopBusinessInfos);
     }
 }
