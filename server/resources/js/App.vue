e0e2ab269a89780b0495cf20df0a615ced0b412b
sgash708 <sugash7081023@gmail.com>
2021-08-26 17:58:11 +0900

    TypeScript実装

---
+++ server/resources/js/App.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        {{ hello }}
+    </div>
+</template>
+
+<script lang="ts">
+import { ref } from 'vue';
+export default{
+    setup() {
+        const hello = ref(
+            "Hello From TypeScript"
+        )
+        return {
+            hello
+        }
+    },
+};
+</script>
