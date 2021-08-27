7d630436b725468f3c5a09cdeb288443d74dc1c6
sgash708 <sugash7081023@gmail.com>
2021-08-27 09:48:12 +0900

    削除リクエストクラス作成

---
+++ server/app/Http/Requests/DeleteUserPost.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Consts\UserConsts;
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreUserPost extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules(): array
+    {
+        return [
+            'id' => 'required|numeric|unique:users',
+        ];
+    }
+
+    /**
+     * trans messages
+     *
+     * @return array
+     */
+    public function messages(): array
+    {
+        return [
+            'id.required' => __('validation.required', ['attribute' => UserConsts::LABEL['id']]),
+            'id.numeric'  => __('validation.numeric', ['attribute' => UserConsts::LABEL['id']]),
+        ];
+    }
+}
