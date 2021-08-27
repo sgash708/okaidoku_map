94c99f9c1262d8a93689a85b56651ed50ae1f5a4
sgash708 <sugash7081023@gmail.com>
2021-08-27 13:08:29 +0900

    route定義

--- server/routes/web.php
+++ server/routes/web.php
@@ -4,6 +4,10 @@ use Illuminate\Support\Facades\Route;
 
 Route::prefix('user')->name('user.')->group(function () {
     Route::get('', 'UserController@index')->name('index');
-    Route::get('/create', 'UserController@create')->name('create');
     Route::post('', 'UserController@store')->name('store');
+    Route::get('/create', 'UserController@create')->name('create');
+
+    // 退会処理
+    Route::post('/confirm', 'UserController@deleteConfirm')->name('delete.confirm');
+    Route::get('/delete', 'UserController@delete')->name('delete.complete');
 });
